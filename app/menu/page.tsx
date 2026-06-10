fca86e3dae08f56f366e38db952fcdc1e6ed229a
Naomi Miura <naomimiura2017@gmail.com>
2026-06-10 12:08:29 -0700

    refactor: Remove debug code

---
+++ app/menu/page.tsx
@@ -0,0 +1,45 @@
+export default function Menu() {
+  return (
+    <main>
+      <h1>Menu </h1>
+      <ul>
+        <li>Appetizers & Snacks</li>
+        <ul>
+          <li>Popcorn</li>
+          <li>Fruit Bowl</li>
+        </ul>
+        <li>Breakfast</li>
+        <ul>
+          <li>Waffles</li>
+          <li>Cereal</li>
+          <li>The Chef Special Breakfast</li>
+        </ul>
+        <li>Lunch</li>
+        <ul>
+          <li>McCuties Nuggets and French Fries</li>
+        </ul>
+        <li>Dinner</li>
+        <ul>
+          <li>Chicken Wings</li>
+          <li>Dumplings</li>
+          <li>Ramen</li>
+          <li>Pizza</li>
+        </ul>
+        <li>Drinks</li>
+        <ul>
+          <li>Cute Milkshake</li>
+          <li>Olipop Flavors</li>
+          <li>Strawberry Drink</li>
+          <li>Yum Yum Yum Yum Yum Yum Yum Yum Yum Boba</li>
+        </ul>
+        <li>Desserts</li>
+        <ul>
+          <li>Yum Yum Cake</li>
+          <li>Jelly</li>
+          <li>Cupcakes</li>
+          <li>Ice Cream</li>
+        </ul>
+      </ul>
+    </main>
+  );
+}
